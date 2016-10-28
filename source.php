a4d32bc2165734700cd227a59e0fdd40df0e3e2b
廖昱信 <lmly9193@gmail.com>
2016-10-29 07:04:06 +0800

    1.0.2
    debug修正畫質片源的完整性

--- source.php
+++ source.php
@@ -1,13 +1,10 @@
 <?php
-	preg_match_all("/([^\|]+)\|/", $response_data, $source);
-	for($i = 1; $i < count($source[0]); $i++) {
+	preg_match("/&fmt_stream_map=.*&fmt_list=/", $response_data, $source_data);
+	preg_match_all("/([^\|]+)\,\d*/", preg_replace("/&fmt_list=/", ",", preg_replace("/&fmt_stream_map=/", Null, implode($source_data))), $source);
+	for($i = 0; $i < count($source[0]); $i++) {
 		$source[1][$i] = preg_replace("/[^\/]+\.googlevideo\.com/", "redirector.googlevideo.com", $source[1][$i]);
 		preg_match("/itag=(?:5|17|18|22|34|35|36|37|38|43|44|45|46|82|84|102|104)/", $source[1][$i], $itag);
 		require("switch.php");
-		if($i == 1) {
-			echo '{file: "'.$source[1][1].'",label: "'.$label.'",type: "video/mp4",default: "true"},';
-		} else {
-			echo '{file: "'.$source[1][$i].'",label: "'.$label.'",type: "video/mp4"},';
-		}
+		echo '{file: "'.$source[1][$i].'",label: "'.$label.'",type: "video/mp4"},';
 	}
 ?>
